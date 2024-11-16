3722bdbe171de37412ea7264461f77608020f8fb
akel <ene.cristian.p7q@student.ucv.ro>
2024-11-16 14:53:48 +0200

    Added Service For Login

---
+++ FitHub.AccountManagement/Features/UserAuth/UserLoginQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using FitHub.AccountManagement.Domain.RegularUser;
+using BCrypt.Net;
+
+namespace FitHub.AccountManagement.Features.UserAuth
+{
+    public class UserLoginQueryHandler(IRegularUserQueryRepository _regularUserQueryRepository)
+    {
+        public async Task<bool> Handle(UserLoginQuery command)
+        {
+            var user = await _regularUserQueryRepository.GetUserByEmail(command.User.Email);
+            return BCrypt.Net.BCrypt.Verify(command.User.Password, user.Password);
+        }
+}

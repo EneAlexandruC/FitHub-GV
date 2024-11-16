44b19022b8c1fadbc4840a81e92823a1bb8fec0e
akel <ene.cristian.p7q@student.ucv.ro>
2024-11-16 19:30:43 +0200

    Fixed API endpoint for register

--- fithub.client/src/utils/api.tsx
+++ fithub.client/src/utils/api.tsx
@@ -4,6 +4,11 @@ import axios from 'axios';
 export const loginAPI = async (credentials : { email: string; password: string }) => {
     throw new Error('Not implemented');
 
-    // const response = await axios.post('/login', credentials);
-    // return response.data;
+    const response = await axios.post('/login', credentials);
+    return response;
+}
+
+export const registerAPI = async (credentials: { firstName: string; lastName: string; email: string; password: string; weight: number; height: number; dateOfBirth: string }) => {
+    const response = await axios.post('https://localhost:7204/api/User/add-regularuser', credentials);
+    return response;
 }

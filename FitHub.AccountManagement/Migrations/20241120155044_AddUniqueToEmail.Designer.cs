1cccc8d0cb65c022d22275d398a63a4697945ef5
akel <ene.cristian.p7q@student.ucv.ro>
2024-11-20 18:59:26 +0200

    Added restriction for email to be unique

--- FitHub.AccountManagement/Migrations/20241120155044_AddUniqueToEmail.Designer.cs
+++ FitHub.AccountManagement/Migrations/20241120155044_AddUniqueToEmail.Designer.cs
@@ -4,6 +4,7 @@ using FitHub.AccountManagement.Infrastructure.UserDataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FitHub.AccountManagement.Migrations
 {
     [DbContext(typeof(RegularUserDbContext))]
-    partial class RegularUserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241120155044_AddUniqueToEmail")]
+    partial class AddUniqueToEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FitHub.AccountManagement/Migrations/20241120155044_AddUniqueToEmail.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitHub.AccountManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueToEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_RegularUser_Email",
+                table: "RegularUser",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RegularUser_Email",
+                table: "RegularUser");
+        }
+    }
+}

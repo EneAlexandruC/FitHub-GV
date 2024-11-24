cd271ed97e14d3c81ff5bb90a0b3fc5ca11604b6
Samuel <margelu_samuel@yahoo.com>
2024-11-24 19:41:44 +0200

    Improved SignUp Page and Home Page(Home Page needs more work)

--- fithub.client/src/App.tsx
+++ fithub.client/src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './features/auth//login/Login';
-import Register from './features/auth/register/Register';
-import Home from './features/home/Home';
-import Nav from './common/nav/Nav';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Login from './features/auth//login/Login';
+import Register from "./features/auth/register/Register";
+import Home from "./features/home/Home";
+import Nav from "./common/nav/Nav";
 
 const App: React.FC = () => {
-
   return (
     <Router>
       <Nav />
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
-
   );
 };
 

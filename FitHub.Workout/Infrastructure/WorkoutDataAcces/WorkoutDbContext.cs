b5017cbbe696cbbbd09c2ec627e8a35cc234b04d
akel <ene.cristian.p7q@student.ucv.ro>
2025-01-03 20:59:21 +0200

    Update namespaces, DbContext, and entity configurations
    Updated `using` directives to reflect new namespaces for `Exercise` and `Workout` classes, and added a new namespace `FitHub.WorkoutManagement.Domain.JoinEntry`.
    
    Added `DbSet<WorkoutExercise>` to `WorkoutDbContext` for managing many-to-many relationships.
    
    Configured `WorkoutExercise` entity in `OnModelCreating` method with composite keys and relationships.
    
    Increased max length of `Description` and `Notes` in `Workout` entity to 1000 characters.
    
    Increased max length of `Name`, `Description`, `Category`, `Muscles`, and `Equipment` in `Exercise` entity to 1000 characters.

--- FitHub.Workout/Infrastructure/WorkoutDataAcces/WorkoutDbContext.cs
+++ FitHub.Workout/Infrastructure/WorkoutDataAcces/WorkoutDbContext.cs
@@ -1,5 +1,6 @@
-﻿using FitHub.WorkoutManagement.Domain.Exercise;
-using FitHub.WorkoutManagement.Domain.Workout;
+﻿using FitHub.WorkoutManagement.Domain.ExerciseDomain;
+using FitHub.WorkoutManagement.Domain.JoinEntry;
+using FitHub.WorkoutManagement.Domain.WorkoutDomain;
 using Microsoft.EntityFrameworkCore;
 
 namespace FitHub.WorkoutManagement.Infrastructure.WorkoutDataAcces
@@ -9,19 +10,29 @@ namespace FitHub.WorkoutManagement.Infrastructure.WorkoutDataAcces
         public WorkoutDbContext(DbContextOptions<WorkoutDbContext> options) : base(options) { }
         public DbSet<Exercise> Exercises { get; set; }
         public DbSet<Workout> Workouts { get; set; }
+        public DbSet<WorkoutExercise> WorkoutExercises { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            // Workout
             modelBuilder.Entity<Workout>(entity =>
             {
                 entity.ToTable("Workout");
                 entity.HasKey(c => c.ID);
 
+                entity.Property(c => c.Description)
+                    .IsRequired()
+                    .HasMaxLength(1000);
+                
                 entity.Property(c => c.Notes)
                     .IsRequired()
-                    .HasMaxLength(30);
+                    .HasMaxLength(1000);
+                
+                entity.Property(c => c.Notes)
+                    .IsRequired()
+                    .HasMaxLength(1000);
             });
 
             // Exercise
@@ -32,29 +43,43 @@ namespace FitHub.WorkoutManagement.Infrastructure.WorkoutDataAcces
 
                 entity.Property(c => c.Name)
                     .IsRequired()
-                    .HasMaxLength(100);
+                    .HasMaxLength(1000);
 
                 entity.Property(c => c.Description)
                     .IsRequired()
-                    .HasMaxLength(100);
+                    .HasMaxLength(1000);
 
                 entity.Property(c => c.Category)
                     .IsRequired()
-                    .HasMaxLength(100);
-                
+                    .HasMaxLength(1000);
+
                 entity.Property(c => c.Muscles)
                     .IsRequired()
-                    .HasMaxLength(100);
+                    .HasMaxLength(1000);
 
                 entity.Property(c => c.Equipment)
                     .IsRequired()
-                    .HasMaxLength(100);
-                
+                    .HasMaxLength(1000);
+
                 entity.Property(c => c.Difficulty)
                     .IsRequired()
                     .HasConversion<int>();
-            }
-            );
+            });
+
+            // WorkoutExercise
+            modelBuilder.Entity<WorkoutExercise>(entity =>
+            {
+                entity.ToTable("WorkoutExercise");
+                entity.HasKey(we => new { we.WorkoutID, we.ExerciseID });
+
+                entity.HasOne(we => we.Workout)
+                    .WithMany(w => w.WorkoutExercises)
+                    .HasForeignKey(we => we.WorkoutID);
+
+                entity.HasOne(we => we.Exercise)
+                    .WithMany(e => e.WorkoutExercises)
+                    .HasForeignKey(we => we.ExerciseID);
+            });
         }
     }
 }

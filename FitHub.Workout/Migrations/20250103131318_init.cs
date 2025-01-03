9b8abc84934fd776fc5dcf075f544f0640245e22
akel <ene.cristian.p7q@student.ucv.ro>
2025-01-03 15:23:14 +0200

    Add initial migration for WorkoutDbContext
    Introduce migration `20250103131318_init` to create `Exercise` and `Workout` tables in `FitHub.WorkoutManagement`. The `Exercise` table includes columns for `ID`, `Name`, `Description`, `Category`, `Muscles`, `Equipment`, and `Difficulty`. The `Workout` table includes columns for `ID`, `Description`, and `Notes`. Update `WorkoutDbContextModelSnapshot` to reflect these changes. Auto-generate `20250103131318_init.Designer.cs` and implement `Up` and `Down` methods in `20250103131318_init.cs`.

---
+++ FitHub.Workout/Migrations/20250103131318_init.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitHub.WorkoutManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Exercise",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Muscles = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Equipment = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Difficulty = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercise", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Workout",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workout", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exercise");
+
+            migrationBuilder.DropTable(
+                name: "Workout");
+        }
+    }
+}
